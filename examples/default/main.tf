9dbfce44c4210ab1cb82c8b8956c10962abbaef0
Vincent Burckhardt <vincent.burckhardt@ie.ibm.com>
2022-08-09 18:29:25 +0100

    fix: fix various issues to get ssh key creation to function

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -3,8 +3,17 @@
 # Copyright 2022 IBM
 #####################################################
 
+# There are discrepencies between the region inputs on the powervs terraform resource, and the vpc ("is") resources
 provider "ibm" {
+  alias            = "ibm-pvs"
   region           = lookup(local.ibm_pvs_zone_region_map, var.pvs_zone, null)
+  zone             = var.pvs_zone
+  ibmcloud_api_key = var.ibmcloud_api_key != null ? var.ibmcloud_api_key : null
+}
+
+provider "ibm" {
+  alias            = "ibm-is"
+  region           = lookup(local.ibm_pvs_zone_cloud_region_map, var.pvs_zone, null)
   zone             = var.pvs_zone
   ibmcloud_api_key = var.ibmcloud_api_key != null ? var.ibmcloud_api_key : null
 }
@@ -26,6 +35,21 @@ locals {
     "sao01"    = "sao"
   }
 
+  ibm_pvs_zone_cloud_region_map = {
+    "syd04"    = "au-syd"
+    "syd05"    = "au-syd"
+    "eu-de-1"  = "eu-de"
+    "eu-de-2"  = "eu-de"
+    "lon04"    = "eu-gb"
+    "lon06"    = "eu-gb"
+    "tok04"    = "jp-tok"
+    "us-east"  = "us-east"
+    "us-south" = "us-south"
+    "dal12"    = "us-south"
+    "tor01"    = "ca-tor"
+    "osa21"    = "jp-osa"
+    "sao01"    = "br-sao"
+  }
 }
 
 resource "tls_private_key" "tls_key" {
@@ -34,11 +58,16 @@ resource "tls_private_key" "tls_key" {
 }
 
 resource "ibm_is_ssh_key" "ssh_key" {
+  provider   = ibm.ibm-is
   name       = var.prefix
-  public_key = tls_private_key.tls_key.public_key_pem
+  public_key = trimspace(tls_private_key.tls_key.public_key_openssh)
 }
 
 module "pvs" {
+  providers = {
+    ibm = ibm.ibm-pvs
+  }
+
   source = "../../"
 
   pvs_zone                 = var.pvs_zone
