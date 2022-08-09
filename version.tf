6138da205af567e1a72e38c6071516f8ac88c0fd
Vincent Burckhardt <vincent.burckhardt@ie.ibm.com>
2022-08-09 18:35:02 +0100

    fix: fix various issues to get ssh key creation to function

--- version.tf
+++ version.tf
@@ -3,20 +3,14 @@
 # Copyright 2022 IBM
 #####################################################
 
-/***************************************************
-NOTE: To source a particular version of IBM terraform provider, configure the parameter `version` as follows
 terraform {
   required_version = ">=1.1"
   required_providers {
+    # Use "greater than or equal to" range in modules
+    # tflint-ignore: terraform_unused_required_providers
     ibm = {
-      source = "IBM-Cloud/ibm"
-      version = "1.21.0"
+      source  = "IBM-Cloud/ibm"
+      version = ">= 1.21.0"
     }
   }
-}
-If we dont configure the version parameter, it fetches the latest provider version.
-****************************************************/
-
-terraform {
-  required_version = ">=1.1"
 }
