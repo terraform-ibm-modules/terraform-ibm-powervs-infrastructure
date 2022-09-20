b30a762c67dd974c3d043845ffc05782b93ffa8b
suraj <suraj.bharadwaj@ibm.com>
2022-09-20 17:03:28 +0200

    feat: rename pvs to powervs

--- submodules/power_service/main.tf
+++ submodules/power_service/main.tf
@@ -8,14 +8,14 @@ locals {
 }
 
 data "ibm_resource_group" "resource_group_ds" {
-  name = var.pvs_resource_group_name
+  name = var.powervs_resource_group_name
 }
 
-resource "ibm_resource_instance" "pvs_service" {
-  name              = var.pvs_service_name
+resource "ibm_resource_instance" "powervs_service" {
+  name              = var.powervs_service_name
   service           = local.service_type
   plan              = local.plan
-  location          = var.pvs_zone
+  location          = var.powervs_zone
   resource_group_id = data.ibm_resource_group.resource_group_ds.id
   tags              = (var.tags != null ? var.tags : [])
 
@@ -31,8 +31,8 @@ resource "ibm_resource_instance" "pvs_service" {
 #####################################################
 
 resource "ibm_pi_key" "ssh_key" {
-  pi_cloud_instance_id = ibm_resource_instance.pvs_service.guid
-  pi_key_name          = var.pvs_sshkey_name
+  pi_cloud_instance_id = ibm_resource_instance.powervs_service.guid
+  pi_key_name          = var.powervs_sshkey_name
   pi_ssh_key           = var.ssh_public_key
 }
 
@@ -41,9 +41,9 @@ resource "ibm_pi_key" "ssh_key" {
 #####################################################
 
 resource "ibm_pi_network" "management_network" {
-  pi_cloud_instance_id = ibm_resource_instance.pvs_service.guid
-  pi_network_name      = var.pvs_management_network["name"]
-  pi_cidr              = var.pvs_management_network["cidr"]
+  pi_cloud_instance_id = ibm_resource_instance.powervs_service.guid
+  pi_network_name      = var.powervs_management_network["name"]
+  pi_cidr              = var.powervs_management_network["cidr"]
   pi_dns               = ["127.0.0.1"]
   pi_network_type      = "vlan"
   pi_network_jumbo     = true
@@ -51,9 +51,9 @@ resource "ibm_pi_network" "management_network" {
 
 resource "ibm_pi_network" "backup_network" {
   depends_on           = [ibm_pi_network.management_network]
-  pi_cloud_instance_id = ibm_resource_instance.pvs_service.guid
-  pi_network_name      = var.pvs_backup_network["name"]
-  pi_cidr              = var.pvs_backup_network["cidr"]
+  pi_cloud_instance_id = ibm_resource_instance.powervs_service.guid
+  pi_network_name      = var.powervs_backup_network["name"]
+  pi_cidr              = var.powervs_backup_network["cidr"]
   pi_dns               = ["127.0.0.1"]
   pi_network_type      = "vlan"
   pi_network_jumbo     = true
@@ -65,16 +65,16 @@ resource "ibm_pi_network" "backup_network" {
 
 data "ibm_pi_catalog_images" "catalog_images_ds" {
   sap                  = true
-  pi_cloud_instance_id = ibm_resource_instance.pvs_service.guid
+  pi_cloud_instance_id = ibm_resource_instance.powervs_service.guid
 }
 
 locals {
-  catalog_images_to_import = flatten([for stock_image in data.ibm_pi_catalog_images.catalog_images_ds.images : [for image_name in var.pvs_image_names : stock_image if stock_image.name == image_name]])
+  catalog_images_to_import = flatten([for stock_image in data.ibm_pi_catalog_images.catalog_images_ds.images : [for image_name in var.powervs_image_names : stock_image if stock_image.name == image_name]])
 }
 
 resource "ibm_pi_image" "import_images" {
-  count                = length(var.pvs_image_names)
-  pi_cloud_instance_id = ibm_resource_instance.pvs_service.guid
+  count                = length(var.powervs_image_names)
+  pi_cloud_instance_id = ibm_resource_instance.powervs_service.guid
   pi_image_id          = local.catalog_images_to_import[count.index].image_id
-  pi_image_name        = var.pvs_image_names[count.index]
+  pi_image_name        = var.powervs_image_names[count.index]
 }
