0e698cbee1d7de32a4ef4703a4660d2567a92567
surajsbharadwaj <101711050+surajsbharadwaj@users.noreply.github.com>
2022-11-16 16:45:56 +0100

    feat: add support for lon04 and lon06 + modify some timeouts (#142)

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -37,8 +37,8 @@ func setupOptions(t *testing.T, prefix string) *testhelper.TestOptions {
 		"reuse_cloud_connections": false,
 		"cloud_connection_count":  1,
 		"cloud_connection_speed":  50,
-		// locking into tok04 until issue #3139 is resolved
-		"powervs_zone": "tok04",
+		// locking into lon06 until issue #3139 is resolved
+		"powervs_zone": "lon06",
 	}
 
 	return options
