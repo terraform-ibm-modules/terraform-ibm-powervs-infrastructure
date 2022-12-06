a3125945a31991d2ddda094ce0a91d5448060bff
suraj bharadwaj <suraj.bharadwaj@ibm.com>
2022-12-06 13:11:25 +0100

    feat: revert to count

--- submodules/power_workspace/main.tf
+++ submodules/power_workspace/main.tf
@@ -78,10 +78,10 @@ locals {
 }
 
 resource "ibm_pi_image" "import_images_1" {
-  for_each             = toset(local.split_images_1)
+  count                = length(local.split_images_1)
   pi_cloud_instance_id = ibm_resource_instance.powervs_workspace.guid
-  pi_image_id          = local.catalog_images_to_import_1[each.value].image_id
-  pi_image_name        = local.catalog_images_to_import_1[each.value].name
+  pi_image_id          = local.catalog_images_to_import_1[count.index].image_id
+  pi_image_name        = local.catalog_images_to_import_1[count.index].name
 
   timeouts {
     create = "9m"
@@ -90,10 +90,10 @@ resource "ibm_pi_image" "import_images_1" {
 
 resource "ibm_pi_image" "import_images_2" {
   depends_on           = [ibm_pi_image.import_images_1]
-  for_each             = toset(local.split_images_2)
+  count                = length(local.split_images_2)
   pi_cloud_instance_id = ibm_resource_instance.powervs_workspace.guid
-  pi_image_id          = local.catalog_images_to_import_2[each.value].image_id
-  pi_image_name        = local.catalog_images_to_import_2[each.value].name
+  pi_image_id          = local.catalog_images_to_import_2[count.index].image_id
+  pi_image_name        = local.catalog_images_to_import_2[count.index].name
 
   timeouts {
     create = "9m"
