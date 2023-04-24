962506547936d4c57028b384cd929bba48f1f008
AnaBil-k <anab@example.com>
2023-04-24 19:09:30 +0200

    refactor: test images import, added AIX images

--- submodules/power_workspace/main.tf
+++ submodules/power_workspace/main.tf
@@ -70,20 +70,26 @@ data "ibm_pi_catalog_images" "catalog_images_ds" {
 
 locals {
 
-  images_length              = length(var.powervs_image_names)
-  split_images_index         = ceil(local.images_length / 2)
-  catalog_images_to_import_1 = flatten([for stock_image in data.ibm_pi_catalog_images.catalog_images_ds.images : [for image_name in slice(var.powervs_image_names, 0, local.split_images_index) : stock_image if stock_image.name == image_name]])
-  catalog_images_to_import_2 = flatten([for stock_image in data.ibm_pi_catalog_images.catalog_images_ds.images : [for image_name in slice(var.powervs_image_names, local.split_images_index, local.images_length) : stock_image if stock_image.name == image_name]])
+  split_images_list = chunklist(var.powervs_image_names, 2)
+  split_images_1    = toset(element(local.split_images_list, 0))
+  split_images_2    = toset(element(local.split_images_list, 1))
+  split_images_3    = toset(element(local.split_images_list, 2))
+
+  catalog_images_to_import_1 = flatten([for stock_image in data.ibm_pi_catalog_images.catalog_images_ds.images : [for image_name in local.split_images_1 : stock_image if stock_image.name == image_name]])
+
+  catalog_images_to_import_2 = flatten([for stock_image in data.ibm_pi_catalog_images.catalog_images_ds.images : [for image_name in local.split_images_2 : stock_image if stock_image.name == image_name]])
+
+  catalog_images_to_import_3 = flatten([for stock_image in data.ibm_pi_catalog_images.catalog_images_ds.images : [for image_name in local.split_images_3 : stock_image if stock_image.name == image_name]])
 
 }
 
 resource "ibm_pi_image" "import_images_1" {
-  for_each = { for idx, val in local.catalog_images_to_import_1 :
-    val.name => val
-  }
+
+  for_each = toset(local.split_images_1)
+
   pi_cloud_instance_id = ibm_resource_instance.powervs_workspace.guid
-  pi_image_id          = each.value.image_id
-  pi_image_name        = each.value.name
+  pi_image_id          = local.catalog_images_to_import_1[index(tolist(local.split_images_1), each.key)].image_id
+  pi_image_name        = each.key
 
   timeouts {
     create = "9m"
@@ -92,14 +98,29 @@ resource "ibm_pi_image" "import_images_1" {
 
 resource "ibm_pi_image" "import_images_2" {
   depends_on = [ibm_pi_image.import_images_1]
-  for_each = { for idx, val in local.catalog_images_to_import_2 :
-    val.name => val
-  }
+
+  for_each = toset(local.split_images_2)
+
   pi_cloud_instance_id = ibm_resource_instance.powervs_workspace.guid
-  pi_image_id          = each.value.image_id
-  pi_image_name        = each.value.name
+  pi_image_id          = local.catalog_images_to_import_2[index(tolist(local.split_images_2), each.key)].image_id
+  pi_image_name        = each.key
 
   timeouts {
     create = "9m"
   }
+}
+
+resource "ibm_pi_image" "import_images_3" {
+  depends_on = [ibm_pi_image.import_images_2]
+
+  for_each = toset(local.split_images_3)
+
+  pi_cloud_instance_id = ibm_resource_instance.powervs_workspace.guid
+  pi_image_id          = local.catalog_images_to_import_3[index(tolist(local.split_images_3), each.key)].image_id
+  pi_image_name        = each.key
+
+  timeouts {
+    create = "9m"
+  }
+
 }
