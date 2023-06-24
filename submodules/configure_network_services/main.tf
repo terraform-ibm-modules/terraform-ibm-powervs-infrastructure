ed85b09c6514dcef39ac675d91a4189149ec788d
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2023-06-24 16:13:38 +0200

    fix: locals BREAKING CHANGE: clear acl rules

--- submodules/configure_network_services/main.tf
+++ submodules/configure_network_services/main.tf
@@ -9,26 +9,16 @@
 locals {
   scr_scripts_dir = "${path.module}/templates"
   dst_scripts_dir = "/root/terraform_scripts"
-
-  src_services_init_tpl_path    = "${local.scr_scripts_dir}/services_init.sh.tftpl"
-  dst_services_init_path        = "${local.dst_scripts_dir}/services_init.sh"
-  src_install_packages_tpl_path = "${local.scr_scripts_dir}/install_packages.sh.tftpl"
-  dst_install_packages_path     = "${local.dst_scripts_dir}/install_packages.sh"
-
-  server_config_option_tmp = merge(var.service_config, { "enable" = true })
-  server_config_options    = { for key, value in local.server_config_option_tmp : key => local.server_config_option_tmp[key] }
-  server_config_name       = split("_", one([for item in keys(var.service_config) : item if can(regex("enable", item))]))[0]
-
-
-  ansible_config_mgmt_svs_playbook_name            = "powervs-services.yml"
-  src_script_configure_network_services_tftpl_path = "${local.scr_scripts_dir}/configure_network_services.sh.tftpl"
-  dst_script_configure_network_services_sh_path    = "${local.dst_scripts_dir}/${local.server_config_name}_config.sh"
-  dst_ansible_vars_path                            = "${local.dst_scripts_dir}/${local.server_config_name}_config.yml"
 }
 
 #####################################################
 # 1. Configure Squid client
 #####################################################
+
+locals {
+  src_services_init_tpl_path = "${local.scr_scripts_dir}/services_init.sh.tftpl"
+  dst_services_init_path     = "${local.dst_scripts_dir}/services_init.sh"
+}
 
 resource "null_resource" "perform_proxy_client_setup" {
 
@@ -129,6 +119,12 @@ resource "time_sleep" "wait_for_reboot" {
 # 3. Install Necessary Packages
 #####################################################
 
+locals {
+
+  src_install_packages_tpl_path = "${local.scr_scripts_dir}/install_packages.sh.tftpl"
+  dst_install_packages_path     = "${local.dst_scripts_dir}/install_packages.sh"
+}
+
 resource "null_resource" "install_packages" {
   depends_on = [time_sleep.wait_for_reboot]
 
@@ -171,9 +167,21 @@ resource "null_resource" "install_packages" {
 }
 
 #####################################################
-# 4. Execute Ansible galaxy role to install service
-# for SAP installation
+# 4. Execute Ansible galaxy role to configure network
+# services (NTP, NFS, DNS)
 #####################################################
+
+locals {
+  server_config_option_tmp = merge(var.service_config, { "enable" = true })
+  server_config_options    = { for key, value in local.server_config_option_tmp : key => local.server_config_option_tmp[key] }
+  server_config_name       = split("_", one([for item in keys(var.service_config) : item if can(regex("enable", item))]))[0]
+
+
+  ansible_configure_network_services_playbook_name = "powervs-services.yml"
+  src_script_configure_network_services_tftpl_path = "${local.scr_scripts_dir}/configure_network_services.sh.tftpl"
+  dst_script_configure_network_services_sh_path    = "${local.dst_scripts_dir}/${local.server_config_name}_config.sh"
+  dst_ansible_vars_path                            = "${local.dst_scripts_dir}/${local.server_config_name}_config.yml"
+}
 
 resource "null_resource" "execute_ansible_role" {
   depends_on = [null_resource.install_packages, null_resource.perform_proxy_client_setup]
@@ -205,7 +213,7 @@ EOF
     content = templatefile(
       local.src_script_configure_network_services_tftpl_path,
       {
-        "ansible_playbook_name" : local.ansible_config_mgmt_svs_playbook_name
+        "ansible_playbook_name" : local.ansible_configure_network_services_playbook_name
         "ansible_extra_vars_path" : local.dst_ansible_vars_path
         "ansible_log_path" : local.dst_scripts_dir
       }
