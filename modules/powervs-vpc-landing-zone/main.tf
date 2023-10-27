b961eeb6044ab26008bdcccdeefe16bab54e5c32
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2023-10-27 08:50:37 +0200

    fix: update terraform terraform-ibm-modules/terraform-worspace/ibm to v1.1.3 BREAKING CHANGE: now import images in parallel. multiple image_import block eliminated

--- modules/powervs-vpc-landing-zone/main.tf
+++ modules/powervs-vpc-landing-zone/main.tf
@@ -48,7 +48,7 @@ module "landing_zone_configure_network_services" {
 
 module "powervs_infra" {
   source    = "terraform-ibm-modules/powervs-workspace/ibm"
-  version   = "1.1.2"
+  version   = "1.1.3"
   providers = { ibm = ibm.ibm-pi }
 
   pi_zone                       = var.powervs_zone
