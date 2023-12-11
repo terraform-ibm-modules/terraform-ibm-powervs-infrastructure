aaea192d3e5c5914fa7e9d809dda06d8b118d242
stafaniasaju <stafania.saju@ibm.com>
2023-12-11 12:12:42 +0100

    feat: ternary operator correction for objects

--- solutions/import-workspace/locals_vpc.tf
+++ solutions/import-workspace/locals_vpc.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 locals {
-  vsi_list  = distinct(flatten([module.access_host.vsi_details, var.access_host.vsi_name != var.proxy_host.vsi_name ? [module.edge_vsi.vsi_detail] : [], var.access_host.vsi_name != var.proxy_host.vsi_name ? [module.workload_vsi.vsi_details] : []]))
+  vsi_list  = distinct(flatten([module.access_host.vsi_details, [[module.edge_vsi.vsi_details], []][var.access_host.vsi_name != var.proxy_host.vsi_name ? 0 : 1], [[module.workload_vsi.vsi_details], []][var.access_host.vsi_name != var.workload_host.vsi_name && var.proxy_host.vsi_name != var.workload_host.vsi_name ? 0 : 1]]))
   vpc_names = distinct([module.access_host.vsi_details.vpc_name, module.edge_vsi.vsi_details.vpc_name, module.workload_vsi.vsi_details.vpc_name])
   vsi_names = distinct([module.access_host.vsi_details.name, module.edge_vsi.vsi_details.name, module.workload_vsi.vsi_details.name])
 }
@@ -31,9 +31,8 @@ locals {
   wrk_common_preset      = jsondecode(local.wrk_common_preset_tpl)
   # access control list rules from presets
   managemnt_vpc_acl_rules = flatten([local.imported_acl_preset.management_acl[0].rules[*], local.mng_common_preset.common_acl[0].rules[*]])
-  #edge_vpc_acl_rules      = var.access_host.vsi_name == var.proxy_host.vsi_name ? local.imported_acl_preset.edge_acl[0].rules[*] : flatten([local.imported_acl_preset.edge_acl[0].rules[*], local.edge_common_preset.common_acl[0].rules[*]])
-  edge_vpc_acl_rules     = [local.imported_acl_preset.edge_acl[0].rules[*], flatten([local.imported_acl_preset.edge_acl[0].rules[*], local.edge_common_preset.common_acl[0].rules[*]])][var.access_host.vsi_name == var.proxy_host.vsi_name ? 0 : 1]
-  workload_vpc_acl_rules = var.access_host.vsi_name == var.workload_host.vsi_name ? local.imported_acl_preset.workload_acl[0].rules[*] : var.proxy_host.vsi_name == var.workload_host.vsi_name ? local.imported_acl_preset.workload_acl[0].rules[*] : flatten([local.imported_acl_preset.workload_acl[0].rules[*], local.wrk_common_preset.common_acl[0].rules[*]])
+  edge_vpc_acl_rules      = [local.imported_acl_preset.edge_acl[0].rules[*], flatten([local.imported_acl_preset.edge_acl[0].rules[*], local.edge_common_preset.common_acl[0].rules[*]])][var.access_host.vsi_name == var.proxy_host.vsi_name ? 0 : 1]
+  workload_vpc_acl_rules  = [local.imported_acl_preset.workload_acl[0].rules[*], [local.imported_acl_preset.workload_acl[0].rules[*], flatten([local.imported_acl_preset.workload_acl[0].rules[*], local.wrk_common_preset.common_acl[0].rules[*]])][var.proxy_host.vsi_name == var.workload_host.vsi_name ? 0 : 1]][var.access_host.vsi_name == var.workload_host.vsi_name ? 0 : 1]
   # list of subnets from each vpc
   management_vsi_subnets = flatten([module.access_host.vsi_ds.primary_network_interface[*].subnet, module.access_host.vsi_ds.network_interfaces[*].subnet])
   edge_vsi_subnets       = flatten([module.edge_vsi.vsi_ds.primary_network_interface[*].subnet, module.edge_vsi.vsi_ds.network_interfaces[*].subnet])
@@ -51,8 +50,7 @@ locals {
   common_sg_rules    = local.imported_sg_preset.common_sg.rules[*]
   managemnt_sg_rules = flatten([local.imported_sg_preset.management_sg.rules[*], local.common_sg_rules])
   edge_sg_rules      = [local.imported_sg_preset.edge_sg.rules[*], flatten([local.imported_sg_preset.edge_sg.rules[*], local.common_sg_rules])][var.access_host.vsi_name == var.proxy_host.vsi_name ? 0 : 1]
-  #edge_sg_rules      = var.access_host.vsi_name == var.proxy_host.vsi_name ? local.imported_sg_preset.edge_sg.rules[*] : flatten([local.imported_sg_preset.edge_sg.rules[*], local.common_sg_rules])
-  workload_sg_rules = var.access_host.vsi_name == var.workload_host.vsi_name ? local.imported_sg_preset.workload_sg.rules[*] : var.proxy_host.vsi_name == var.workload_host.vsi_name ? local.imported_sg_preset.workload_sg.rules[*] : flatten([local.imported_sg_preset.workload_sg.rules[*], local.common_sg_rules])
+  workload_sg_rules  = [local.imported_sg_preset.workload_sg.rules[*], [local.imported_sg_preset.workload_sg.rules[*], flatten([local.imported_sg_preset.workload_sg.rules[*], local.common_sg_rules])][var.proxy_host.vsi_name == var.workload_host.vsi_name ? 0 : 1]][var.access_host.vsi_name == var.workload_host.vsi_name ? 0 : 1]
   # list of security groups from each VSI
   management_sgs = distinct(flatten([module.access_host.vsi_ds.primary_network_interface[*].security_groups, module.access_host.vsi_ds.network_interfaces[*].security_groups]))
   edge_sgs       = distinct(flatten([module.edge_vsi.vsi_ds.primary_network_interface[*].security_groups, module.edge_vsi.vsi_ds.network_interfaces[*].security_groups]))
