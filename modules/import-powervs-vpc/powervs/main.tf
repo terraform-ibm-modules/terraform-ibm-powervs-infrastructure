6d55f0634d418e83a5c60a3a922e992334ce8776
stafaniasaju <stafania.saju@ibm.com>
2024-01-17 13:18:34 +0100

    feat: removing unused output

--- modules/import-powervs-vpc/powervs/main.tf
+++ modules/import-powervs-vpc/powervs/main.tf
@@ -1,16 +1,8 @@
-data "ibm_pi_workspace" "powervs_workspace" {
+data "ibm_pi_workspace" "powervs_workspace_ds" {
   pi_cloud_instance_id = var.pi_workspace_guid
-}
-
-data "ibm_resource_instance" "powervs_workspace_ds" {
-  name = data.ibm_pi_workspace.powervs_workspace.pi_workspace_name
 }
 
 data "ibm_pi_images" "powervs_workspace_images_ds" {
-  pi_cloud_instance_id = var.pi_workspace_guid
-}
-
-data "ibm_pi_cloud_connections" "cloud_connections" {
   pi_cloud_instance_id = var.pi_workspace_guid
 }
 
--- modules/import-powervs-vpc/powervs/outputs.tf
+++ modules/import-powervs-vpc/powervs/outputs.tf
@@ -1,33 +1,24 @@
 output "powervs_workspace_name" {
   description = "The name of the PowerVS workspace."
-  value       = data.ibm_pi_workspace.powervs_workspace.pi_workspace_name
+  value       = data.ibm_pi_workspace.powervs_workspace_ds.pi_workspace_name
 }
 
-output "powervs_workspace_guid" {
-  description = "The GUID of PowerVS workspace."
-  value       = data.ibm_resource_instance.powervs_workspace_ds.guid
+output "powervs_workspace_id" {
+  description = "PowerVS infrastructure workspace ID."
+  value       = data.ibm_pi_workspace.powervs_workspace_ds.id
 }
 
 output "powervs_workspace_crn" {
   description = "PowerVS infrastructure workspace CRN."
-  value       = data.ibm_resource_instance.powervs_workspace_ds.crn
+  value       = data.ibm_pi_workspace.powervs_workspace_ds.pi_workspace_details.crn
 }
 
-output "powervs_resource_group_name" {
-  description = "IBM Cloud resource group in which PowerVS infrastructure exists."
-  value       = data.ibm_resource_instance.powervs_workspace_ds.resource_group_name
-}
 
 output "powervs_images" {
   description = "Object containing imported PowerVS image names and image ids."
   value = {
     for image in data.ibm_pi_images.powervs_workspace_images_ds.image_info : image.name => image.id
   }
-}
-
-output "cloud_connections_count" {
-  description = "The data of the cloud connections which attcahed to the Power Virtual Server Workspace."
-  value       = length(data.ibm_pi_cloud_connections.cloud_connections.connections)
 }
 
 output "powervs_management_network_subnet" {
