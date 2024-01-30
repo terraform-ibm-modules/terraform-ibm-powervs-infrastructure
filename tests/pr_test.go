e87475acd01d05ee6f0feccdf42ee2b4a344a129
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2024-01-30 14:27:09 +0100

    ci: add regions prefs.yaml file

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -42,7 +42,11 @@ func setupOptions(t *testing.T, prefix string) *testhelper.TestOptions {
 		ResourceGroup:      resourceGroup,
 		Region:             "us-south", // specify default region to skip best choice query
 		DefaultRegion:      "us-south",
-		BestRegionYAMLPath: "../common-dev-assets/common-go-assets/cloudinfo-region-power-prefs.yaml", // specific to powervs zones
+		BestRegionYAMLPath: "./common-go-assets/cloudinfo-region-power-prefs.yaml", // specific to powervs zones
+		// temporary workaround for BSS backend issue
+		ImplicitDestroy: []string{
+			"module.fullstack.module.landing_zone.module.landing_zone.ibm_resource_group.resource_groups",
+		},
 	})
 
 	// query for best zone to deploy powervs example, based on current connection count
