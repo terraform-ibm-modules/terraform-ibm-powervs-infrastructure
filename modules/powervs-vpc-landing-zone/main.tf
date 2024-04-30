d37d7660decb963d443963077b26f895fffe2d2c
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2024-04-30 16:12:54 +0200

    feat: switch syd04 to PER DC SKIP UPGRADE TEST: skipping because of default images

--- modules/powervs-vpc-landing-zone/main.tf
+++ modules/powervs-vpc-landing-zone/main.tf
@@ -48,7 +48,7 @@ module "landing_zone_configure_network_services" {
 
 module "powervs_infra" {
   source    = "terraform-ibm-modules/powervs-workspace/ibm"
-  version   = "1.11.0"
+  version   = "1.12.0"
   providers = { ibm = ibm.ibm-pi }
 
   pi_zone                       = var.powervs_zone
