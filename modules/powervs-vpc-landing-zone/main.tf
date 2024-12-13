afe54769b81ee3bec8dc034d0407fd8f4e5e5861
surajsbharadwaj <101711050+surajsbharadwaj@users.noreply.github.com>
2024-12-13 13:32:11 +0100

    feat: Support dal14 and lon04 DC (#973)
    * feat:  Support mad04 DC for quickstart using P10

--- modules/powervs-vpc-landing-zone/main.tf
+++ modules/powervs-vpc-landing-zone/main.tf
@@ -81,7 +81,7 @@ locals {
 
 module "powervs_workspace" {
   source    = "terraform-ibm-modules/powervs-workspace/ibm"
-  version   = "2.2.0"
+  version   = "2.3.0"
   providers = { ibm = ibm.ibm-pi }
 
   pi_zone                                 = var.powervs_zone
