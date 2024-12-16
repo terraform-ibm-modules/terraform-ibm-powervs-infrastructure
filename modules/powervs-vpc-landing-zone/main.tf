806ff1470d841b301dc8401f7d436609840156c6
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2024-12-16 11:14:52 +0100

    chore: update modules; ci dependencies; tf version

--- modules/powervs-vpc-landing-zone/main.tf
+++ modules/powervs-vpc-landing-zone/main.tf
@@ -4,7 +4,7 @@
 
 module "landing_zone" {
   source    = "terraform-ibm-modules/landing-zone/ibm//patterns//vsi//module"
-  version   = "6.6.1"
+  version   = "6.5.1"
   providers = { ibm = ibm.ibm-is }
 
   ssh_public_key       = var.ssh_public_key
