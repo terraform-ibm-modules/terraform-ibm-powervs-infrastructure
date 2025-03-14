75bfcad909281ca2bf54298bc67447987694f0bc
ludwig-mueller <ludwig.mueller@ibm.com>
2025-03-14 21:42:30 +0100

    fix: pi instance name was not using prefix (#1014)
    * chore(deps): update terraform ibm to latest for deployable architectures
    * chore(deps): update terraform-module
    * chore(deps): update ci dependencies
    * fix: pi instance name was not using prefix
    * chore: change location for validation
    
    ---------
    Co-authored-by: surajsbharadwaj <suraj.bharadwaj@ibm.com>

--- solutions/standard-plus-vsi/main.tf
+++ solutions/standard-plus-vsi/main.tf
@@ -40,7 +40,7 @@ module "standard" {
 
 module "powervs_instance" {
   source    = "terraform-ibm-modules/powervs-instance/ibm"
-  version   = "2.4.1"
+  version   = "2.4.2"
   providers = { ibm = ibm.ibm-pi }
 
   pi_workspace_guid      = module.standard.powervs_workspace_guid
