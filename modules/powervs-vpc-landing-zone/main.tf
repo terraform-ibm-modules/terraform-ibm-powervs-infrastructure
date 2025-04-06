0f9db38f772d9a9443f255c2c7ff4511a1f3ec1f
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-04-06 03:52:17 +0100

    chore(deps): update terraform-module (#1035)

--- modules/powervs-vpc-landing-zone/main.tf
+++ modules/powervs-vpc-landing-zone/main.tf
@@ -18,7 +18,7 @@ locals {
 
 module "landing_zone" {
   source    = "terraform-ibm-modules/landing-zone/ibm//patterns//vsi//module"
-  version   = "7.4.2"
+  version   = "7.4.3"
   providers = { ibm = ibm.ibm-is }
 
   ssh_public_key       = var.ssh_public_key
@@ -57,7 +57,7 @@ locals {
 
 module "scc_wp_instance" {
   source    = "terraform-ibm-modules/scc-workload-protection/ibm"
-  version   = "1.5.4"
+  version   = "1.5.6"
   providers = { ibm = ibm.ibm-is }
   count     = var.enable_scc_wp ? 1 : 0
 
