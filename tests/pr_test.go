a68f7e0d7380b6968208cd1fbdcbab05977ce592
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2025-05-07 14:40:02 +0200

    chore: implicit destroy for ws and subnets

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -60,6 +60,11 @@ func setupOptionsStandardSolution(t *testing.T, prefix string, powervs_zone stri
 		Prefix:        prefix,
 		ResourceGroup: resourceGroup,
 		Region:        powervs_zone,
+		ImplicitDestroy: []string{
+			"module.standard.module.powervs_workspace.ibm_resource_instance.pi_workspace",
+			"module.standard.module.powervs_workspace.ibm_pi_network.private_subnet_1[0]",
+			"module.standard.module.powervs_workspace.ibm_pi_network.private_subnet_2[0]",
+		},
 	})
 
 	options.TerraformVars = map[string]interface{}{
