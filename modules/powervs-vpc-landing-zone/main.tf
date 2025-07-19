e376d088c054d8d49c9b9972ace2eb19b0e66160
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-07-19 13:39:53 +0000

    chore(deps): update terraform dependencies

--- modules/powervs-vpc-landing-zone/main.tf
+++ modules/powervs-vpc-landing-zone/main.tf
@@ -59,7 +59,7 @@ locals {
 # Create new App Config instance
 module "app_config" {
   source    = "terraform-ibm-modules/app-configuration/ibm"
-  version   = "1.8.7"
+  version   = "1.8.9"
   providers = { ibm = ibm.ibm-is }
   count     = var.enable_scc_wp ? 1 : 0
 
