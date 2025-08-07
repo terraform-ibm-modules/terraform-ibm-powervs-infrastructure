0d0cb3ec6bead16c7ab3ed7075b1a310103193d8
ludwig-mueller <ludwig.mueller@ibm.com>
2025-08-07 14:30:10 +0200

    refactor: split main.tf into separate files for each functional unit SKIP UPGRADE TEST

--- modules/powervs-vpc-landing-zone/main.tf
+++ modules/powervs-vpc-landing-zone/main.tf
@@ -2,7 +2,6 @@
 # VPC Landing Zone module
 #####################################################
 locals {
-
   external_access_ip = var.external_access_ip != null && var.external_access_ip != "" ? length(regexall("/", var.external_access_ip)) > 0 ? var.external_access_ip : "${var.external_access_ip}/32" : ""
   # Openshift IPI requires VPC resources, PowerVS resources, and TGW to be in the same resource group
   second_rg_name = var.powervs_resource_group_name != null ? "slz-edge-rg" : "ocp-rg"
@@ -21,6 +20,7 @@ locals {
   )
 }
 
+
 module "landing_zone" {
   source    = "terraform-ibm-modules/landing-zone/ibm//patterns//vsi//module"
   version   = "8.4.3"
@@ -32,78 +32,6 @@ module "landing_zone" {
   override_json_string = local.override_json_string
 }
 
-#####################################################
-# IBM Cloud Monitoring Instance module
-#####################################################
-
-resource "ibm_resource_instance" "monitoring_instance" {
-  count             = var.enable_monitoring && var.existing_monitoring_instance_crn == null ? 1 : 0
-  provider          = ibm.ibm-is
-  name              = "${var.prefix}-monitoring-instance"
-  location          = lookup(local.ibm_powervs_zone_cloud_region_map, var.powervs_zone, null)
-  service           = "sysdig-monitor"
-  plan              = "graduated-tier"
-  resource_group_id = module.landing_zone.resource_group_data["${var.prefix}-slz-service-rg"]
-  tags              = var.tags
-}
-
-locals {
-  monitoring_instance = {
-    enable             = var.enable_monitoring
-    crn                = var.enable_monitoring && var.existing_monitoring_instance_crn == null ? resource.ibm_resource_instance.monitoring_instance[0].crn : var.existing_monitoring_instance_crn != null ? var.existing_monitoring_instance_crn : ""
-    location           = var.enable_monitoring && var.existing_monitoring_instance_crn == null ? resource.ibm_resource_instance.monitoring_instance[0].location : var.existing_monitoring_instance_crn != null ? split(":", var.existing_monitoring_instance_crn)[5] : ""
-    guid               = var.enable_monitoring && var.existing_monitoring_instance_crn == null ? resource.ibm_resource_instance.monitoring_instance[0].guid : var.existing_monitoring_instance_crn != null ? split(":", var.existing_monitoring_instance_crn)[7] : ""
-    monitoring_host_ip = local.monitoring_vsi_ip
-  }
-}
-
-#################################################
-# SCC Workload Protection Instance module
-#################################################
-
-# Create new App Config instance
-module "app_config" {
-  source    = "terraform-ibm-modules/app-configuration/ibm"
-  version   = "1.8.12"
-  providers = { ibm = ibm.ibm-is }
-  count     = var.enable_scc_wp ? 1 : 0
-
-  region                                 = lookup(local.ibm_powervs_zone_cloud_region_map, var.powervs_zone, null)
-  resource_group_id                      = module.landing_zone.resource_group_data["${var.prefix}-slz-service-rg"]
-  app_config_plan                        = "basic"
-  app_config_name                        = "${var.prefix}-app-config"
-  app_config_tags                        = var.tags
-  enable_config_aggregator               = true
-  config_aggregator_trusted_profile_name = "${var.prefix}-app-config-tp"
-}
-
-module "scc_wp_instance" {
-  source    = "terraform-ibm-modules/scc-workload-protection/ibm"
-  version   = "1.10.13"
-  providers = { ibm = ibm.ibm-is }
-  count     = var.enable_scc_wp ? 1 : 0
-
-  name                                         = "${var.prefix}-scc-wp-instance"
-  region                                       = lookup(local.ibm_powervs_zone_cloud_region_map, var.powervs_zone, null)
-  resource_group_id                            = module.landing_zone.resource_group_data["${var.prefix}-slz-service-rg"]
-  scc_wp_service_plan                          = "graduated-tier"
-  scc_workload_protection_trusted_profile_name = "${var.prefix}-workload-protection-trusted-profile"
-  resource_tags                                = var.tags
-  resource_key_name                            = "${var.prefix}-scc-wp-manager-key"
-  resource_key_tags                            = var.tags
-  cloud_monitoring_instance_crn                = local.monitoring_instance.crn != "" ? local.monitoring_instance.crn : null
-  app_config_crn                               = var.enable_scc_wp ? module.app_config[0].app_config_crn : null
-}
-
-locals {
-  scc_wp_instance = {
-    enable             = var.enable_scc_wp
-    guid               = var.enable_scc_wp ? module.scc_wp_instance[0].guid : "",
-    access_key         = var.enable_scc_wp ? nonsensitive(module.scc_wp_instance[0].access_key) : "",
-    api_endpoint       = var.enable_scc_wp ? nonsensitive(replace(module.scc_wp_instance[0].api_endpoint, "https://", "https://private.")) : "",
-    ingestion_endpoint = var.enable_scc_wp ? nonsensitive(replace(module.scc_wp_instance[0].ingestion_endpoint, "ingest.", "ingest.private.")) : ""
-  }
-}
 
 # ###########################################################
 # # Routing table used by NLB for NFS and VPN
@@ -117,54 +45,6 @@ resource "ibm_is_vpc_routing_table" "routing_table" {
   vpc                              = [for vpc in module.landing_zone.vpc_data : vpc.vpc_id if vpc.vpc_name == "${var.prefix}-edge"][0]
   route_transit_gateway_ingress    = true
   accept_routes_from_resource_type = var.client_to_site_vpn.enable ? ["vpn_server"] : []
-}
-
-###########################################################
-# PowerVS Workspace module
-###########################################################
-
-locals {
-  powervs_custom_image1 = (
-    var.powervs_custom_images.powervs_custom_image1.image_name == "" &&
-    var.powervs_custom_images.powervs_custom_image1.file_name == "" &&
-    var.powervs_custom_images.powervs_custom_image1.storage_tier == ""
-  ) ? null : var.powervs_custom_images.powervs_custom_image1
-  powervs_custom_image2 = (
-    var.powervs_custom_images.powervs_custom_image2.image_name == "" &&
-    var.powervs_custom_images.powervs_custom_image2.file_name == "" &&
-    var.powervs_custom_images.powervs_custom_image2.storage_tier == ""
-  ) ? null : var.powervs_custom_images.powervs_custom_image2
-  powervs_custom_image3 = (
-    var.powervs_custom_images.powervs_custom_image3.image_name == "" &&
-    var.powervs_custom_images.powervs_custom_image3.file_name == "" &&
-    var.powervs_custom_images.powervs_custom_image3.storage_tier == ""
-  ) ? null : var.powervs_custom_images.powervs_custom_image3
-  powervs_custom_image_cos_configuration = (
-    var.powervs_custom_image_cos_configuration.bucket_name == "" &&
-    var.powervs_custom_image_cos_configuration.bucket_access == "" &&
-    var.powervs_custom_image_cos_configuration.bucket_region == ""
-  ) ? null : var.powervs_custom_image_cos_configuration
-}
-
-module "powervs_workspace" {
-  source  = "terraform-ibm-modules/powervs-workspace/ibm"
-  version = "3.2.0"
-
-  providers = { ibm = ibm.ibm-pi }
-
-  pi_zone                                 = var.powervs_zone
-  pi_resource_group_name                  = var.powervs_resource_group_name
-  pi_workspace_name                       = "${var.prefix}-${var.powervs_zone}-power-workspace"
-  pi_ssh_public_key                       = { "name" = "${var.prefix}-${var.powervs_zone}-pvs-ssh-key", value = var.ssh_public_key }
-  pi_private_subnet_1                     = var.powervs_management_network
-  pi_private_subnet_2                     = var.powervs_backup_network
-  pi_transit_gateway_connection           = { "enable" : true, "transit_gateway_id" : module.landing_zone.transit_gateway_data.id }
-  pi_tags                                 = var.tags
-  pi_custom_image1                        = local.powervs_custom_image1
-  pi_custom_image2                        = local.powervs_custom_image2
-  pi_custom_image3                        = local.powervs_custom_image3
-  pi_custom_image_cos_configuration       = local.powervs_custom_image_cos_configuration
-  pi_custom_image_cos_service_credentials = var.powervs_custom_image_cos_service_credentials
 }
 
 
@@ -192,8 +72,8 @@ locals {
       "fstype"          = "nfs4"
     }
   }
-
 }
+
 
 module "configure_network_services" {
 
@@ -224,63 +104,4 @@ module "configure_network_services" {
   src_inventory_template_name = "inventory.tftpl"
   dst_inventory_file_name     = "network-services-instance-inventory"
   inventory_template_vars     = { "host_or_ip" : local.network_services_vsi_ip }
-}
-
-
-module "configure_monitoring_host" {
-
-  source     = "./submodules/ansible"
-  depends_on = [module.configure_network_services]
-  count      = var.enable_monitoring ? 1 : 0
-
-  bastion_host_ip        = local.access_host_or_ip
-  ansible_host_or_ip     = local.network_services_vsi_ip
-  ssh_private_key        = var.ssh_private_key
-  configure_ansible_host = false
-
-  src_script_template_name = "configure-monitoring-instance/ansible_exec.sh.tftpl"
-  dst_script_file_name     = "monitoring-instance.sh"
-
-  src_playbook_template_name = "configure-monitoring-instance/playbook-configure-monitoring-instance.yml.tftpl"
-  dst_playbook_file_name     = "monitoring-instance-playbook.yml"
-  playbook_template_vars = {
-    "client_config" : jsonencode(
-      {
-        "nfs" : local.network_services_config.nfs
-        "dns" : { enable = var.configure_dns_forwarder, dns_server_ip = local.network_services_vsi_ip }
-        "ntp" : { enable = var.configure_ntp_forwarder, ntp_server_ip = local.network_services_vsi_ip }
-    })
-  }
-
-  src_inventory_template_name = "inventory.tftpl"
-  dst_inventory_file_name     = "monitoring-instance-inventory"
-  inventory_template_vars     = { "host_or_ip" : local.monitoring_vsi_ip }
-}
-
-
-module "configure_scc_wp_agent" {
-
-  source     = "./submodules/ansible"
-  depends_on = [module.configure_network_services, module.configure_monitoring_host]
-  count      = var.enable_scc_wp ? 1 : 0
-
-  bastion_host_ip        = local.access_host_or_ip
-  ansible_host_or_ip     = local.network_services_vsi_ip
-  ssh_private_key        = var.ssh_private_key
-  ansible_vault_password = var.ansible_vault_password
-  configure_ansible_host = false
-
-  src_script_template_name = "configure-scc-wp-agent/ansible_configure_scc_wp_agent.sh.tftpl"
-  dst_script_file_name     = "${var.prefix}-configure_scc_wp_agent.sh"
-
-  src_playbook_template_name = "configure-scc-wp-agent/playbook-configure-scc-wp-agent-linux.yml.tftpl"
-  dst_playbook_file_name     = "${var.prefix}-playbook-configure-scc-wp-agent.yml"
-  playbook_template_vars = {
-    COLLECTOR_ENDPOINT : local.scc_wp_instance.ingestion_endpoint,
-    API_ENDPOINT : local.scc_wp_instance.api_endpoint,
-    ACCESS_KEY : local.scc_wp_instance.access_key
-  }
-  src_inventory_template_name = "inventory.tftpl"
-  dst_inventory_file_name     = "${var.prefix}-scc-wp-inventory"
-  inventory_template_vars     = { "host_or_ip" : join("\n", [for vsi in module.landing_zone.vsi_list : vsi["ipv4_address"]]) }
 }
---
+++ modules/powervs-vpc-landing-zone/monitoring.tf
@@ -0,0 +1,59 @@
+#####################################################
+# IBM Cloud Monitoring Instance module
+#####################################################
+
+resource "ibm_resource_instance" "monitoring_instance" {
+  count             = var.enable_monitoring && var.existing_monitoring_instance_crn == null ? 1 : 0
+  provider          = ibm.ibm-is
+  name              = "${var.prefix}-monitoring-instance"
+  location          = lookup(local.ibm_powervs_zone_cloud_region_map, var.powervs_zone, null)
+  service           = "sysdig-monitor"
+  plan              = "graduated-tier"
+  resource_group_id = module.landing_zone.resource_group_data["${var.prefix}-slz-service-rg"]
+  tags              = var.tags
+}
+
+locals {
+  monitoring_instance = {
+    enable             = var.enable_monitoring
+    crn                = var.enable_monitoring && var.existing_monitoring_instance_crn == null ? resource.ibm_resource_instance.monitoring_instance[0].crn : var.existing_monitoring_instance_crn != null ? var.existing_monitoring_instance_crn : ""
+    location           = var.enable_monitoring && var.existing_monitoring_instance_crn == null ? resource.ibm_resource_instance.monitoring_instance[0].location : var.existing_monitoring_instance_crn != null ? split(":", var.existing_monitoring_instance_crn)[5] : ""
+    guid               = var.enable_monitoring && var.existing_monitoring_instance_crn == null ? resource.ibm_resource_instance.monitoring_instance[0].guid : var.existing_monitoring_instance_crn != null ? split(":", var.existing_monitoring_instance_crn)[7] : ""
+    monitoring_host_ip = local.monitoring_vsi_ip
+  }
+}
+
+#####################################################
+# Configure monitoring VSI
+# VSI is created in landing zone preset
+#####################################################
+
+module "configure_monitoring_host" {
+
+  source     = "./submodules/ansible"
+  depends_on = [module.configure_network_services]
+  count      = var.enable_monitoring ? 1 : 0
+
+  bastion_host_ip        = local.access_host_or_ip
+  ansible_host_or_ip     = local.network_services_vsi_ip
+  ssh_private_key        = var.ssh_private_key
+  configure_ansible_host = false
+
+  src_script_template_name = "configure-monitoring-instance/ansible_exec.sh.tftpl"
+  dst_script_file_name     = "monitoring-instance.sh"
+
+  src_playbook_template_name = "configure-monitoring-instance/playbook-configure-monitoring-instance.yml.tftpl"
+  dst_playbook_file_name     = "monitoring-instance-playbook.yml"
+  playbook_template_vars = {
+    "client_config" : jsonencode(
+      {
+        "nfs" : local.network_services_config.nfs
+        "dns" : { enable = var.configure_dns_forwarder, dns_server_ip = local.network_services_vsi_ip }
+        "ntp" : { enable = var.configure_ntp_forwarder, ntp_server_ip = local.network_services_vsi_ip }
+    })
+  }
+
+  src_inventory_template_name = "inventory.tftpl"
+  dst_inventory_file_name     = "monitoring-instance-inventory"
+  inventory_template_vars     = { "host_or_ip" : local.monitoring_vsi_ip }
+}
