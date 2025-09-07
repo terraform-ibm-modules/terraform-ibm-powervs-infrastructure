0774c6feb81e1546f7690ec720b9ac39af1d22e7
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-09-07 08:11:31 +0100

    chore(deps): update dependencies (#1150)

--- modules/powervs-vpc-landing-zone/client2sitevpn.tf
+++ modules/powervs-vpc-landing-zone/client2sitevpn.tf
@@ -127,7 +127,7 @@ module "secrets_manager_private_certificate" {
 # Create client to site VPN Server
 module "client_to_site_vpn" {
   source    = "terraform-ibm-modules/client-to-site-vpn/ibm"
-  version   = "3.2.25"
+  version   = "3.2.26"
   providers = { ibm = ibm.ibm-is }
   count     = var.client_to_site_vpn.enable ? 1 : 0
 
