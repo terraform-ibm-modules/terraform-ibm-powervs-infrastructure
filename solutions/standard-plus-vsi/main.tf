6e72dde8b8e0caf1a0c16f73ffc68e8dd56b207e
ludwig-mueller <ludwig.mueller@ibm.com>
2025-09-26 12:03:41 +0200

    fix: upgrade instance module to 2.8.1

--- solutions/standard-plus-vsi/main.tf
+++ solutions/standard-plus-vsi/main.tf
@@ -55,7 +55,7 @@ module "standard" {
 
 module "powervs_instance" {
   source    = "terraform-ibm-modules/powervs-instance/ibm"
-  version   = "2.8.0"
+  version   = "2.8.1"
   providers = { ibm = ibm.ibm-pi }
 
   pi_workspace_guid      = module.standard.powervs_workspace_guid
