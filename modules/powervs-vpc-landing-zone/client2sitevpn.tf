edd5f5e39773d26ddd85a5134159b7d791c5a611
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-25 05:48:49 +0000

    chore(deps): update dependencies

--- modules/powervs-vpc-landing-zone/client2sitevpn.tf
+++ modules/powervs-vpc-landing-zone/client2sitevpn.tf
@@ -91,7 +91,7 @@ resource "ibm_resource_instance" "secrets_manager" {
 # Configure private cert engine if provisioning a new SM instance
 module "private_secret_engine" {
   source     = "terraform-ibm-modules/secrets-manager-private-cert-engine/ibm"
-  version    = "1.10.1"
+  version    = "1.10.2"
   providers  = { ibm = ibm.ibm-sm }
   count      = var.client_to_site_vpn.enable ? 1 : 0
   depends_on = [ibm_resource_instance.secrets_manager]
