2783a0bb880e878d8c19810cdf8ff44a521e831f
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-11-15 07:30:12 +0000

    chore(deps): update dependencies

--- modules/powervs-vpc-landing-zone/main.tf
+++ modules/powervs-vpc-landing-zone/main.tf
@@ -28,7 +28,7 @@ locals {
 
 module "landing_zone" {
   source    = "terraform-ibm-modules/landing-zone/ibm//patterns//vsi//module"
-  version   = "8.12.2"
+  version   = "8.12.4"
   providers = { ibm = ibm.ibm-is }
 
   ssh_public_key       = var.ssh_public_key
