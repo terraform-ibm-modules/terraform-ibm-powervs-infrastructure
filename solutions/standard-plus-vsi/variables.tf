f137a9407d31c60e9f04bab9df4d8be3d415842a
ludwig-mueller <ludwig.mueller@ibm.com>
2025-11-20 16:04:20 +0100

    feat: introduce ocp tshirts and cluster destroy automation (#1193)
    * fix: make it so ansible only reinstalls modules if it detects an outdated version
    
    * chore(deps): update instance to v2.8.3
    
    * feat: prototype ocp tshirts
    
    * chore(docs): improve variable descriptions
    
    * refactor: move locals to new file
    
    * feat: update tshirt sizes based on Michael's recommendations
    
    * chore: change order of the tiles, put standard last
    
    * feat: destroy automation with variable trigger
    
    * fix: remove notes, add quotes to cpu count
    
    * fix: add count to cluster deployment playbooks so they're destroyed when the cluster is destroyed. This allows to deploy the cluster again
    
    * fix: switch to only full cpu cores until we figure out why fractional cores are causing an error
    
    * docs: toc version and lastupdated
    
    * chore: add validation for processors to only support full numbers
    
    * chore: improve tshirt displayname

--- solutions/standard-plus-vsi/variables.tf
+++ solutions/standard-plus-vsi/variables.tf
@@ -17,7 +17,7 @@ variable "tshirt_size" {
 
   validation {
     condition     = contains(["custom", "aix_xs", "aix_s", "aix_m", "aix_l", "ibm_i_xs", "ibm_i_s", "ibm_i_m", "ibm_i_l", "sap_dev_rhel", "sap_dev_sles"], var.tshirt_size.tshirt_size)
-    error_message = "Only Following DC values are supported :  custom, aix_xs, aix_s, aix_m, aix_l, ibm_i_xs, ibm_i_s, ibm_i_m, ibm_i_l, sap_dev_rhel, sap_dev_sles"
+    error_message = "Only Following values are supported :  custom, aix_xs, aix_s, aix_m, aix_l, ibm_i_xs, ibm_i_s, ibm_i_m, ibm_i_l, sap_dev_rhel, sap_dev_sles"
   }
 }
 
