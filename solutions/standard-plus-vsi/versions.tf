b5d54759457a54428198201e114e46a2f898dbd0
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-16 08:16:36 +0000

    chore(deps): update dependencies (#1202)
    * chore(deps): update dependencies
    
    * chore: add short_description attribute for each flavor
    
    ---------
    
    Co-authored-by: Suraj Bharadwaj <101711050+surajsbharadwaj@users.noreply.github.com>
    Co-authored-by: ludwig-mueller <ludwig.mueller@ibm.com>

--- solutions/standard-plus-vsi/versions.tf
+++ solutions/standard-plus-vsi/versions.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.85.0"
+      version = "1.86.0"
     }
     restapi = {
       source  = "Mastercard/restapi"
