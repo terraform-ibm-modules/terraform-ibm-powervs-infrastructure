aaca1575c90ecbf986b32f0c1e3174ae8115795d
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-20 08:13:53 +0000

    chore(deps): update dependencies (#1204)

--- modules/powervs-vpc-landing-zone/scc-wp.tf
+++ modules/powervs-vpc-landing-zone/scc-wp.tf
@@ -5,7 +5,7 @@
 # Create new App Config instance
 module "app_config" {
   source    = "terraform-ibm-modules/app-configuration/ibm"
-  version   = "1.14.2"
+  version   = "1.14.4"
   providers = { ibm = ibm.ibm-is }
   count     = var.enable_scc_wp ? 1 : 0
 
@@ -20,7 +20,7 @@ module "app_config" {
 
 module "scc_wp_instance" {
   source    = "terraform-ibm-modules/scc-workload-protection/ibm"
-  version   = "1.16.4"
+  version   = "1.16.7"
   providers = { ibm = ibm.ibm-is }
   count     = var.enable_scc_wp ? 1 : 0
 
