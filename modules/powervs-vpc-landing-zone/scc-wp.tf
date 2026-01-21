75b3d29c70be8d90af02c50531e88c9ddd1f7365
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-21 15:42:28 +0000

    fix: update dependencies remove version constraints (#1227)

--- modules/powervs-vpc-landing-zone/scc-wp.tf
+++ modules/powervs-vpc-landing-zone/scc-wp.tf
@@ -5,7 +5,7 @@
 # Create new App Config instance
 module "app_config" {
   source    = "terraform-ibm-modules/app-configuration/ibm"
-  version   = "1.14.9"
+  version   = "1.14.11"
   providers = { ibm = ibm.ibm-is }
   count     = var.enable_scc_wp ? 1 : 0
 
@@ -20,7 +20,7 @@ module "app_config" {
 
 module "scc_wp_instance" {
   source    = "terraform-ibm-modules/scc-workload-protection/ibm"
-  version   = "1.16.19"
+  version   = "1.16.21"
   providers = { ibm = ibm.ibm-is }
   count     = var.enable_scc_wp ? 1 : 0
 
