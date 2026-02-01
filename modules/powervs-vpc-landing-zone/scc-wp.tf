e1952e49c5d9707c8706a13ca81a2bd7aec7fb0d
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-02-01 08:14:17 +0000

    chore(deps): update terraform terraform-ibm-modules/app-configuration/ibm to v1.15.4 (#1235)

--- modules/powervs-vpc-landing-zone/scc-wp.tf
+++ modules/powervs-vpc-landing-zone/scc-wp.tf
@@ -5,7 +5,7 @@
 # Create new App Config instance
 module "app_config" {
   source    = "terraform-ibm-modules/app-configuration/ibm"
-  version   = "1.15.2"
+  version   = "1.15.4"
   providers = { ibm = ibm.ibm-is }
   count     = var.enable_scc_wp ? 1 : 0
 
