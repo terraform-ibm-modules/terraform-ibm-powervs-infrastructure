de1b174e4ec755ff106a8443b22ecb3afdc64b48
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-02-01 09:07:26 +0000

    chore(deps): update terraform terraform-ibm-modules/landing-zone/ibm to v8.14.20

--- modules/powervs-vpc-landing-zone/main.tf
+++ modules/powervs-vpc-landing-zone/main.tf
@@ -29,7 +29,7 @@ locals {
 
 module "landing_zone" {
   source    = "terraform-ibm-modules/landing-zone/ibm//patterns//vsi//module"
-  version   = "8.14.19"
+  version   = "8.14.20"
   providers = { ibm = ibm.ibm-is }
 
   ssh_public_key       = var.ssh_public_key
