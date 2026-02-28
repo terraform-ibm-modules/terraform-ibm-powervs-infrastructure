9738a9ad0662115dfcddee63b943ce996fe92097
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-02-28 15:30:39 +0000

    chore(deps): update dependencies

--- solutions/standard-plus-vsi/versions.tf
+++ solutions/standard-plus-vsi/versions.tf
@@ -7,11 +7,11 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.88.2"
+      version = "1.88.3"
     }
     restapi = {
       source  = "Mastercard/restapi"
-      version = "2.0.1"
+      version = "3.0.0"
     }
   }
 }
