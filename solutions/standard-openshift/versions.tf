0744623fe31a60256082a3ed78d2e5d04c97c1fa
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-03-10 08:43:36 +0000

    chore(deps): update dependencies (#1247)

--- solutions/standard-openshift/versions.tf
+++ solutions/standard-openshift/versions.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.88.2"
+      version = "1.89.0"
     }
     restapi = {
       source  = "Mastercard/restapi"
