542fd7913238e12d3c21e46a4b2b84668b03ca44
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2026-03-20 19:33:26 +0100

    fix: change test location SKIP UPGRADE TEST: test standard plus VSI solution

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -97,7 +97,7 @@ func setupOptionsStandardSolution(t *testing.T, prefix string, powervs_zone stri
 func TestRunBranchStandardExample(t *testing.T) {
 	t.Parallel()
 
-	options := setupOptionsStandardSolution(t, "pvs-i-b", "us-east")
+	options := setupOptionsStandardSolution(t, "pvs-i-b", "syd05")
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
@@ -106,7 +106,7 @@ func TestRunBranchStandardExample(t *testing.T) {
 
 func TestRunMainStandardExample(t *testing.T) {
 	t.Parallel()
-	options := setupOptionsStandardSolution(t, "pvs-i-m", "us-south")
+	options := setupOptionsStandardSolution(t, "pvs-i-m", "sao01")
 
 	output, err := options.RunTestUpgrade()
 	if !options.UpgradeTestSkipped {
