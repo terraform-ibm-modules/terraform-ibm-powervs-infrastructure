7ab43a322c68b383384183374dc0180d244ca243
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2026-09-02 18:00:27 +0200

    chore: update test region

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -99,7 +99,7 @@ func setupOptionsStandardSolution(t *testing.T, prefix string, powervs_zone stri
 func TestRunBranchStandardExample(t *testing.T) {
 	t.Parallel()
 
-	options := setupOptionsStandardSolution(t, "pvs-i-b", "syd04")
+	options := setupOptionsStandardSolution(t, "pvs-i-b", "mad04")
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
